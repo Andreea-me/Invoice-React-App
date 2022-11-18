49e927427015e26c95889e72f21f4a92d2a97cc0
Andreea-me <dinuandreeae@gmail.com>
2022-11-18 15:37:15 +0200

    this is a good version

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,21 +6,19 @@ import "./App.css";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <ul className="sidebarList">
+      <ul className="sidebarList" style={{textDecorationLine: "none"}}>
         {SidebarData.map((val, key) => {
           return (
             <>
               <li
                 key={key}
-                className="row"
                 id={window.location.pathname === val.link ? "active" : ""}
-                // onClick={() => {
-                //   window.location.pathname = val.link;
-                // }}
               >
                 <NavLink to={val.link}>
-                  <div id="icon">{val.icon}</div>
-                  <div id="title">{val.title}</div>
+                  <div className="row">
+                    <div id="icon">{val.icon}</div>
+                    <div id="title">{val.title}</div>
+                  </div>
                 </NavLink>
               </li>
             </>
